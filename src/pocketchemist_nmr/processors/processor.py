2a2fedabf05ff18ae6079cf9eec0c8a2488b49d2
J Lorieau <justin@lorieau.com>
2022-01-11 15:55:15 -0600

    Implement FTSpectra processor and required parameters

--- src/pocketchemist_nmr/processors/processor.py
+++ src/pocketchemist_nmr/processors/processor.py
@@ -16,9 +16,14 @@ class LoadSpectra(NMRProcessor):
     # The loaded NMR spectra
     spectra = None
 
-    required_params = ('in_filepath',)
+    required_params = ('in_filepath', 'format')
 
 
 class FTSpectra(NMRProcessor):
     """Fourier Transform spectra (one or more)"""
-    
+
+    #: Fourier Transform mode
+    #: - 'auto': determine which method to use based on the spectra
+    #: - 'inv': Fourier Transform a frequency spectrum to a time-domain
+    #: - 'real': Real Fourier Transform
+    required_params = ('mode',)
