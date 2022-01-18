e90f3df344bf3a7bd7e00b88622881fb46ab97dc
J Lorieau <justin@lorieau.com>
2022-01-18 11:57:44 -0600

    Reorganize NMRPipeSpectrum tests and imports

--- src/pocketchemist_nmr/__init__.py
+++ src/pocketchemist_nmr/__init__.py
@@ -1,5 +1,1 @@
-from . import cli, processors, spectra
-
-__all__ = ('cli', 'processors', 'spectra')
-
 __version__ = '0.1.0'
--- tests/spectra/test_nmrpipe_spectrum.py
+++ tests/spectra/test_nmrpipe_spectrum.py
@@ -2,6 +2,7 @@
 Test the spectra/nmrpipe_spectrum.py submodule
 """
 from pathlib import Path
+from itertools import repeat
 
 import pytest
 import numpy as np
@@ -16,6 +17,53 @@ spectrum2d_exs = (Path('data') / 'bruker' /
 spectrum3d_exs = (Path('data') / 'bruker' /
                   'CD20170124_av500hd_103_ubq_hncosi2d' /
                   'fid' / 'test%03d.fid',)
+
+
+# Property Accessors/Mutators
+@pytest.mark.parametrize("in_filepath,ndims",
+                         [(spec, 2) for spec in spectrum2d_exs] +
+                         [(spec, 3) for spec in spectrum3d_exs])
+def test_nmrpipe_spectrum_ndims_nd(in_filepath, ndims):
+    """Test the NMRPipeSpectrum ndims property (nD)"""
+    # Load the spectrum and check the number of dimensions
+    spectrum = NMRPipeSpectrum(in_filepath)
+    assert spectrum.ndims == ndims
+
+    # See if the number of dimensions is correctly set in the header
+    assert int(spectrum.meta['FDDIMCOUNT']) == ndims
+
+    # For 1D and 2D datasets, the number of dimensions can be read from
+    # self.data. (This can't be done for 3D, 4D and so on because the data
+    # represents an iterator
+    del spectrum.meta['FDDIMCOUNT']
+    if ndims < 3:
+        assert spectrum.ndims == ndims
+    else:
+        with pytest.raises(AttributeError):
+            spectrum.ndims
+
+
+@pytest.mark.parametrize("in_filepath,expected_order",
+                         [(spec, (1, 2)) for spec in spectrum2d_exs] +
+                         [(spec, (2, 1, 3)) for spec in spectrum3d_exs])
+def test_nmrpipe_spectrum_order_2d(in_filepath, expected_order):
+    """Test the NMRPipeSpectrum order property (2D)"""
+    # Load the spectrum and check the number of dimensions
+    spectrum = NMRPipeSpectrum(in_filepath)
+
+    # Check the default order
+    assert spectrum.order == expected_order
+
+    # Try changing the order
+    spectrum.order = tuple(reversed(expected_order))
+    assert spectrum.order == tuple(reversed(expected_order))
+
+    # Check the header
+    # ex: [2.0, 1.0, 3.0, 4.0] for a 2D
+    fddimorder = ([float(i) for i in spectrum.order] +
+                  [float(i) for i in range(len(expected_order) + 1, 4 + 1)])
+    assert spectrum.meta['FDDIMORDER'] == fddimorder
+
 
 # I/O methods
 
@@ -44,10 +92,22 @@ def test_nmrpipe_spectrum_load_3d(in_filepath):
     # Check that the spectrum was properly setup
     assert isinstance(spectrum.meta, dict)
     assert isinstance(spectrum.data, ng.fileio.pipe.iter3D)
+    assert spectrum.is_iterator
 
     # Check the header
     assert 'FDDIMCOUNT' in spectrum.meta
     assert int(spectrum.meta['FDDIMCOUNT']) == 3
+
+    # Try iterating
+    spectrum2d = next(spectrum)
+    assert type(spectrum2d) == type(spectrum)
+    assert isinstance(spectrum2d.meta, dict)
+    assert isinstance(spectrum2d.data, np.ndarray)
+    assert not spectrum2d.is_iterator
+
+    # Check the header
+    assert 'FDDIMCOUNT' in spectrum2d.meta
+    assert int(spectrum2d.meta['FDDIMCOUNT']) == 3
 
 
 def test_nmrpipe_spectrum_load_missing_2d():
@@ -81,31 +141,4 @@ def test_nmrpipe_spectrum_reset_2d(in_filepath):
     assert spectrum.data is None
 
 
-# Accessors/Mutators
-
-@pytest.mark.parametrize("in_filepath", spectrum2d_exs)
-def test_nmrpipe_spectrum_ndims_2d(in_filepath):
-    """Test the NMRPipeSpectrum ndims property (2D)"""
-    # Load the spectrum and check the number of dimensions
-    spectrum = NMRPipeSpectrum(in_filepath)
-    assert spectrum.ndims == 2
-
-    # Try removing the header entry and see if the number of dimensions is
-    # correctly read out
-    del spectrum.meta['FDDIMCOUNT']
-    assert spectrum.ndims == 2
-
-
-@pytest.mark.parametrize("in_filepath", spectrum2d_exs)
-def test_nmrpipe_spectrum_ndims_2d(in_filepath):
-    """Test the NMRPipeSpectrum order property (2D)"""
-    # Load the spectrum and check the number of dimensions
-    spectrum = NMRPipeSpectrum(in_filepath)
-
-    # Check the default order
-    assert spectrum.order == (1, 2)
-
-    # Try changing the order
-    spectrum.order = (2, 1)
-    assert spectrum.order == (2, 1)
-    assert spectrum.meta['FDDIMORDER'] == [2.0, 1.0, 3.0, 4.0]
+# Mutators/Processing methods
