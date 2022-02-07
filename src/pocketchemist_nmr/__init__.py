6778701a7be34b69a2c6293768126d1397edd244
J Lorieau <justin@lorieau.com>
2022-02-07 09:42:10 -0600

    Version bump to 0.1.1

--- src/pocketchemist_nmr/__init__.py
+++ src/pocketchemist_nmr/__init__.py
@@ -1,5 +1,7 @@
+import importlib.metadata
+
 from . import cli, processors, spectra
 
 __all__ = ('cli', 'processors', 'spectra')
 
-__version__ = '0.1.0'
+__version__ = importlib.metadata.version("pocketchemist_nmr")
