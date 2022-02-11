20cc9ec478e697b5fabe6aa706cad0301402054d
J Lorieau <justin@lorieau.com>
2022-02-11 11:03:36 -0600

    Add data layout constants

--- src/pocketchemist_nmr/spectra/constants.py
+++ src/pocketchemist_nmr/spectra/constants.py
@@ -3,7 +3,7 @@ Constants and enum types for NMRSpectra
 """
 from enum import Enum
 
-__all__ = ('DomainType',)
+__all__ = ('DomainType', 'DataType', 'DataLayout')
 
 
 # Enumeration types
@@ -20,3 +20,11 @@ class DataType(Enum):
     REAL = 1  # Real data
     IMAG = 2  # Imaginary data
     COMPLEX = 3  # Complex data containing real and imag components
+
+
+class DataLayout(Enum):
+    """How the data is layed out in a dimension"""
+
+    CONTIGUOUS = 0  # No interleaving
+    SINGLE_INTERLEAVE = 10  # Complex real/imag points adjacent to each other
+    BLOCK_INTERLEAVE = 20  # Complex real points followed by imag points
