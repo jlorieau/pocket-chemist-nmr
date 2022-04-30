46c6ae192b6bcc7bf220d9ff325e179eb70b0898
J Lorieau <justin@lorieau.com>
2022-04-30 08:19:11 -0500

    Add vtrace and htrace mouse interaction modes

--- src/pocketchemist_nmr/gui/constants.py
+++ src/pocketchemist_nmr/gui/constants.py
@@ -1,11 +1,17 @@
 from enum import Enum
 
 
-class MouseMode(Enum):
-    """The mouse navigation and selection states."""
+class MouseInteraction(Enum):
+    """The mouse interaction states for navigation and selection"""
 
     #: The default mode to pan and zoom
     NAVIGATION = 'Navigation'
 
     #: Add peaks mode
     ADDPEAKS = 'Add Peaks'
+
+    #: Horizontal 1D trace mode
+    HTRACE = "Horizontal 1D"
+
+    #: Vertical 1D trace mode
+    VTRACE = "Vertical 1D"
